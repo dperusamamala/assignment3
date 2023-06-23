c647ade13cd5cb8106aae35a5c59dbc492ffc9ff
dperusamamala <136485481+dperusamamala@users.noreply.github.com>
2023-06-23 04:51:49 -0700

    Add files via upload

---
+++ dart/main.dart
@@ -0,0 +1,20 @@
+void main() {
+  //question; 1
+  List<String> names = ['Usama', 'Muhammad Ismail', 'Yousuf', 'Muzzammil'];
+  print(names);
+
+  //question; 2
+
+  List<String> days = [];
+
+  days.addAll([
+    'saturday',
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday'
+  ]);
+  print(days);
+}
